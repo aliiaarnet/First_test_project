6c26485c0b725e8872b16b753335b8a01e9e9afb
admin <aliia.arnet@gmail.com>
2020-01-06 12:00:07 +0300

    spanish was changed to english

--- conftest.py
+++ conftest.py
@@ -4,14 +4,14 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose browser: es or fr")
+                     help="Choose browser: en or fr")
 
 @pytest.fixture(scope="function")
 def browser(request):
     print("\nstart browser for test..")
     language = request.config.getoption("language")
     browser = None
-    if language == "es":
+    if language == "en":
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
@@ -20,7 +20,7 @@ def browser(request):
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
     else:
-        raise pytest.UsageError("--language should be es or fr")
+        raise pytest.UsageError("--language should be en or fr")
     yield browser
     print("\nquit browser..")
     browser.quit()
