88332caa5f04730a94d4958276eeb5ac383cd375
admin <aliia.arnet@gmail.com>
2020-01-07 20:00:15 +0300

    Commented out code was uncommented

--- test_main_page.py
+++ test_main_page.py
@@ -5,17 +5,17 @@ import time
 from .Pages.main_page import MainPage
 from .Pages.login_page import LoginPage
 
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#
-# def test_guest_should_see_login_link(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 def test_guest_should_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com"
