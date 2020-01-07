28c2db98b0a61f96fcd5b3e3dae85bcd5b8e4041
admin <aliia.arnet@gmail.com>
2020-01-07 19:52:54 +0300

    Tests for login page were added

--- test_main_page.py
+++ test_main_page.py
@@ -3,16 +3,40 @@ from selenium import webdriver
 import pytest
 import time
 from .Pages.main_page import MainPage
+from .Pages.login_page import LoginPage
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
+#
+# def test_guest_should_see_login_link(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    time.sleep(3)
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_form()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+def test_guest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_register_form()
+
+def test_guest_should_see_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
